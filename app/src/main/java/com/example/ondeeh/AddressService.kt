a78bfabbf83af4792b3d70733502412c86dbbcf3
Bruno Oliveira <brunohosilva97@gmail.com>
2023-03-04 17:21:14 -0300

    :tada: Cep project

---
+++ app/src/main/java/com/example/ondeeh/AddressService.kt
@@ -0,0 +1,8 @@
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface AddressService {
+    @GET("/ws/{cep}/json")
+    fun search(@Path("cep") cep: String): Call<Address>
+}
